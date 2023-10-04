3f859ca95374602289d9be665476ef6636a939ca
BRUGUSOFT <kiran.paturi@brugu.io>
2023-10-04 15:32:28 +0530

    added ce

--- demoapp/src/App.jsx
+++ demoapp/src/App.jsx
@@ -1,24 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Dropdown } from "./components/Dropdown";
 import { Header } from "./components/Header";
 import { Textbox } from "./components/Textbox";
 import { register } from "./data/register";
+import { getData, getDataByAxios } from "./services";
 
 export const App =()=>{
   const nameOfTheCompany='Medtronic';
   const [profile,setProfile] = useState({FirstName:"",LastName:""})
+  const [countryList,setCountryList]= useState([]);
   const handleChange = (name,value)=>{
     let content = profile;
     content[name]=value;
     setProfile({...content});
   };
-  const countryList =[{
-    value:"IN",
-    text:"India"
-  },{
-    value:"USA",
-    text:"United States"
-  }]
+  useEffect(()=>{
+    const getCountryList = async()=>{
+      try{
+         let result = await getDataByAxios("https://restcountries.com/v2/all");
+          let convertedResult = result.map(x=>{
+            return {text:x.name,value:x.alpha2Code}
+          })
+         setCountryList(convertedResult);
+      }catch(ex){
+        console.log(ex)
+      }
+    };
+    getCountryList()
+
+  },[])
+  //getData("https://restcountries.com/v2/all")
+  
+  // const countryList =[{
+  //   value:"IN",
+  //   text:"India"
+  // },{
+  //   value:"USA",
+  //   text:"United States"
+  // }]
   return(
   <div>
     <Header companyName={nameOfTheCompany}/>
---
+++ demoapp/src/services/index.js
@@ -0,0 +1,21 @@
+import axios from "axios"
+export const getData = (url) => {
+    fetch(url).then(result => {
+        console.log(result)
+        return result.json()
+    }).then(response => {
+        console.log(response)
+        return response;
+    }).catch(err => {
+        console.log(err);
+    })
+}
+export const getDataByAxios = async (url) => {
+    try {
+        const result = await axios.get(url);
+        return result.data;
+    } catch (ex) {
+        console.log(ex);
+        return ex;
+    }
+}
